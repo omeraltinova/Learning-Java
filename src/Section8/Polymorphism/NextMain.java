3462ac687be356dbf392bbda323c02aa199d22ba
Ömer Faruk Altınova <omerfarukaltinova@gmail.com>
2025-08-12 16:23:06 +0300

    Refactor movie watching logic to handle unknown movie types dynamically

--- src/Section8/Polymorphism/NextMain.java
+++ src/Section8/Polymorphism/NextMain.java
@@ -2,17 +2,27 @@ package Section8.Polymorphism;
 
 public class NextMain {
     public static void main(String[] args) {
-        Movie theMovie = Movie.getMovie("A","Jaws");
-        theMovie.watchMovie();
+//        Movie theMovie = Movie.getMovie("A", "Jaws");
+//        theMovie.watchMovie();
+//
+//        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+//        jaws.watchMovie();
+//
+//        Object comedy = Movie.getMovie("C", "Airplane");
+//        Comedy comedyMovie = (Comedy) comedy;
+//        comedyMovie.watchComedy();
+//
+//        var airplane = Movie.getMovie("C", "Airplane");
+//        airplane.watchMovie();
 
-        Adventure jaws = (Adventure) Movie.getMovie("A","Jaws");
-        jaws.watchMovie();
-
-        Object comedy = Movie.getMovie("C","Airplane");
-        Comedy comedyMovie = (Comedy) comedy;
-        comedyMovie.watchComedy();
-
-        var airplane = Movie.getMovie("C","Airplane");
-        airplane.watchMovie();
+        Object unknownObject = Movie.getMovie("S", "Star Wars");
+        if (unknownObject.getClass().getSimpleName() == "Comedy") {
+            Comedy c = (Comedy) unknownObject;
+            c.watchComedy();
+        } else if(unknownObject instanceof Adventure){
+            ((Adventure) unknownObject).watchAdventure();
+        } else if(unknownObject instanceof ScienceFiction syfy){
+            syfy.watchScienceFiction();
+        }
     }
 }
