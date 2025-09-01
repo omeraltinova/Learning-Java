636fd6d788540721f13905663c988bc0464c7c09
Ömer Faruk Altınova <88210573+omeraltinova@users.noreply.github.com>
2025-09-01 17:01:08 +0300

    Add Main and MoreLists classes for grocery item management and manipulation

---
+++ src/Section10/ArrayLists/Challange/Challange2/MobilePhone.java
@@ -0,0 +1,68 @@
+package Section10.ArrayLists.Challange.Challange2;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact) >= 0) {
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int foundPosition = findContact(oldContact);
+        if (foundPosition < 0) {
+            return false;
+        }
+        myContacts.set(foundPosition, newContact);
+        return true;
+    }
+
+    public boolean removeContact(Contact contact) {
+        int foundPosition = findContact(contact);
+        if (foundPosition < 0) {
+            return false;
+        }
+        myContacts.remove(foundPosition);
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return findContact(contact.getName());
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i < myContacts.size(); i++) {
+            Contact contact = myContacts.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Contact queryContact(String name) {
+        int position = findContact(name);
+        if (position >= 0) {
+            return myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List:");
+        for (int i = 0; i < myContacts.size(); i++) {
+            System.out.println((i + 1) + ". " + myContacts.get(i).getName() + " -> " + myContacts.get(i).getPhoneNumber());
+        }
+    }
+}
