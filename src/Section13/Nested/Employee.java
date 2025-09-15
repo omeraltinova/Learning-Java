2ba41ee01bc70805b5d3a51b37633b916fcc71b0
Ömer Faruk Altınova <88210573+omeraltinova@users.noreply.github.com>
2025-09-15 19:43:55 +0300

    Add Employee and StoreEmployee classes; implement sorting functionality in Main

---
+++ src/Section13/Nested/Employee.java
@@ -0,0 +1,51 @@
+package Section13.Nested;
+
+
+import java.util.Comparator;
+
+public class Employee {
+    public static class EmployeeComparator <T extends Employee> implements Comparator<Employee> {
+
+
+        private String sortType;
+
+        public EmployeeComparator(String sortType) {
+            this.sortType = sortType;
+        }
+
+        public EmployeeComparator() {
+            this("name");
+        }
+
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            if(sortType.equalsIgnoreCase("yearStarted")){
+                return o1.yearStarted-o2.yearStarted;
+            }
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+
+
+    private int employeeId;
+    private String name;
+    private int yearStarted;
+
+    public Employee() {
+    }
+
+    public Employee(int employeeId, String name, int yearStarted) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.yearStarted = yearStarted;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "%d %-8s %d".formatted(employeeId,name,yearStarted);
+    }
+}
---
+++ src/Section13/Nested/burger/Store.java
@@ -0,0 +1,9 @@
+package Section13.Nested.burger;
+
+public class Store {
+
+    public static void main(String[] args) {
+        Meal regularMeal = new Meal();
+        System.out.println(regularMeal);
+    }
+}
