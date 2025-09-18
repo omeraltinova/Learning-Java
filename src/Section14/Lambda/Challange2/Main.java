68d2d5f1c846159db8a8a0ed9b6aee92efd64e6b
Ömer Faruk Altınova <88210573+omeraltinova@users.noreply.github.com>
2025-09-18 20:20:15 +0300

    Add Main class with string manipulation examples using lambda expressions

---
+++ src/Section14/Lambda/Challange2/Main.java
@@ -0,0 +1,46 @@
+package Section14.Lambda.Challange2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+
+        String[] names = {"Anna","Bob","Carole","David","Ed","Fred","Gary"};
+
+        Arrays.setAll(names, i-> names[i].toUpperCase());
+        System.out.println("Transform to uppercase");
+        System.out.println(Arrays.toString(names));
+
+        List<String> backedByArray = Arrays.asList(names);
+
+        backedByArray.replaceAll(s -> s += " " + getRandomChar('A', 'D') + ".");
+        System.out.println("--> Add random middle initial");
+        System.out.println(Arrays.toString(names));
+
+        backedByArray.replaceAll(s -> s += " " + getReversedName(s.split(" ")[0]));
+        System.out.println("--> Add reversed name as last name");
+        Arrays.asList(names).forEach(s -> System.out.println(s));
+
+        List<String> newList = new ArrayList<>(List.of(names));
+        newList.removeIf(s -> s.substring(0, s.indexOf(" ")).equals(
+                s.substring(s.lastIndexOf(" ") + 1)
+        ));
+        System.out.println("--> Remove names where first name equals last name");
+        newList.forEach(s -> System.out.println(s));
+
+    }
+
+    public static char getRandomChar(char startChar,char endChar){
+        return (char) random.nextInt((int) startChar,(int) endChar+1);
+    }
+    private static String getReversedName(String firstName){
+        return new StringBuilder(firstName).reverse().toString();
+    }
+}
